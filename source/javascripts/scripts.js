62e4258137bc081a57e53f6b9e105e83b57d0646
Alex Lauderdale <alauderdale@Alexs-MacBook-Pro.local>
2018-07-16 01:03:39 -0600

    more

--- source/javascripts/scripts.js
+++ source/javascripts/scripts.js
@@ -4,5 +4,19 @@ $(document).ready(function(){
         $( "html" ).toggleClass( "nav-open" );
     });
 
+    $( ".menu-link" ).click(function() {
+        $('.menu-button').trigger('click');
+    });
+
+
+
+
+	var scroll = new SmoothScroll('.scroller-link', {
+
+		offset: 80,
+
+	});
+
+
 
 });
